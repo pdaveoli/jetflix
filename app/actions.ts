8112b4ac4911477fc2275caa56a3fca1c009ca24
Oliver Dave <pdaveoli@monkswalk.herts.sch.uk>
2025-02-11 10:30:07 +0000

    New api setup

--- app/actions.ts
+++ app/actions.ts
@@ -1,15 +1,2 @@
-const { MovieDb } = require('moviedb-promise')
-const moviedb = new MovieDb('095d10d1bdc8d14ed4bc2ccb2447710b')
-
-const findMovie = async (title: string) => {
-    // Equivalant to { query: title }
-    const res = await moviedb.searchMovie(title)
-  
-    return res
-}
-
-const findMovieByGenre = async (genre: string) => {
-    const res = await moviedb.genreMovieList({ id: genre })
-  
-    return res
-}
+import { TMDB } from 'tmdb-ts';
+const tmdb = new TMDB('095d10d1bdc8d14ed4bc2ccb2447710b');
