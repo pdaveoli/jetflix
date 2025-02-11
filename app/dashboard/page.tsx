f9967e44c078dd5e19f556261c9de34006cee42c
Oliver Dave <pdaveoli@monkswalk.herts.sch.uk>
2025-02-11 10:15:22 +0000

    Moved components directroy

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import { UserDetails } from "../components/user-details";
+import { UserDetails } from "../../components/user-details";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
-import { CodeSwitcher } from "../components/code-switcher";
-import { LearnMore } from "../components/learn-more";
-import { Footer } from "../components/footer";
-import { ClerkLogo } from "../components/clerk-logo";
-import { NextLogo } from "../components/next-logo";
+import { CodeSwitcher } from "../../components/code-switcher";
+import { LearnMore } from "../../components/learn-more";
+import { Footer } from "../../components/footer";
+import { ClerkLogo } from "../../components/clerk-logo";
+import { NextLogo } from "../../components/next-logo";
 
 import { DASHBOARD_CARDS } from "../consts/cards";
 
