cbb2a29853e4ef630e8caade8b6d6b7c452368d9
Oliver Dave <pdaveoli@monkswalk.herts.sch.uk>
2025-03-04 10:45:46 +0000

    Image posters (:

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -69,7 +69,7 @@ export default async function Dashboard({ films} : DashboardProps) {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
               {films.map((film) => (
                 <div key={film.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-                  <img src={"https://media.themoviedb.org/t/p/w300_and_h450_bestv2/" + film.poster_path} alt={film.title} className="w-full h-48 object-cover" />
+                  <img src={"https://media.themoviedb.org/t/p/w300_and_h450_bestv2/" + film.poster_path} alt={film.title} className="w-300 h-450 object-cover" />
                   <div className="p-4">
                     <h3 className="text-lg font-semibold">{film.title}</h3>
                     <div className="flex justify-between mt-2">
