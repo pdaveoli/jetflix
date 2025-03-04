76a2c62c7ccdbfbff71018277bf46f78e45f3d3e
Oliver Dave <pdaveoli@monkswalk.herts.sch.uk>
2025-03-04 10:56:55 +0000

    pageNumber url

--- app/dashboard/getMovies.ts
+++ app/dashboard/getMovies.ts
@@ -1,6 +1,8 @@
 import { getMovies } from "../actions";
 
-export async function fetchMovies() {
-    const films = (await getMovies()).results;
-    return films;
+export async function fetchMovies(pageNumber: number) {
+    const films = (await getMovies(pageNumber));
+    
+    
+    return films.results;
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import Dashboard from "@/components/dashboard";
 import { fetchMovies } from "./getMovies";
+import { useSearchParams } from 'next/navigation'
 
 export default async function DashboardPage() {
-
-  const films = await fetchMovies();
-  
-
+  const searchParams = useSearchParams();
+  const pageNumber = searchParams.get('page') ? parseInt(searchParams.get('page') as string) : 0;
+  const films = await fetchMovies(pageNumber);
 
   return <Dashboard films={films} />;
 }
