cd5a0c4fb55b068144d3170467d8b1652bb25ef0
Oliver Dave <pdaveoli@monkswalk.herts.sch.uk>
2025-05-20 09:27:10 +0000

    feat: Remove experimental turbotrace configuration from Next.js config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    turbotrace: {
-      memoryLimit: 4096,
-    },
-  },
   images: {
     remotePatterns: [
       {
